8df2d6dd94df943f9cb2a837559b604bd98ab5ca
vkorovkin <vkorovkin@gmail.com>
2020-10-15 01:16:39 +0300

    small fix

--- kotlin_mpp_lib_reuse_problem/LibX/LibX/src/commonMain/kotlin/libx/LibXBaseClass.kt
+++ kotlin_mpp_lib_reuse_problem/LibX/LibX/src/commonMain/kotlin/libx/LibXBaseClass.kt
@@ -1,7 +1,4 @@
 package libx
 
-/**
- *
- * created by vkorovkin  on 14.10.2020
- */
+
 open class LibXBaseClass(val name: String)
--- kotlin_mpp_lib_reuse_problem/LibXConsumer/LibXConsumer/src/commonMain/kotlin/libxconsumer/LibXBaseClassUsageExample.kt
+++ kotlin_mpp_lib_reuse_problem/LibXConsumer/LibXConsumer/src/commonMain/kotlin/libxconsumer/LibXBaseClassUsageExample.kt
@@ -1,8 +1,5 @@
 package libxconsumer
 
-/**
- *
- * created by vkorovkin  on 14.10.2020
- */
+
 class LibXBaseClassUsageExample : libx.LibXBaseClass("Xyz") {
 }
