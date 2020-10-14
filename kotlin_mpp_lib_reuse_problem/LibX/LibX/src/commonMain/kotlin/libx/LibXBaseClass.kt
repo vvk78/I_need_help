9230cef0a0b1edd3fc2e1832f08478e73614f934
vkorovkin <vkorovkin@gmail.com>
2020-10-15 01:09:17 +0300

    Kotlin mpp lib sharing problem illustration added

---
+++ kotlin_mpp_lib_reuse_problem/LibX/LibX/src/commonMain/kotlin/libx/LibXBaseClass.kt
@@ -0,0 +1,7 @@
+package libx
+
+/**
+ *
+ * created by vkorovkin  on 14.10.2020
+ */
+open class LibXBaseClass(val name: String)
---
+++ kotlin_mpp_lib_reuse_problem/LibXConsumer/LibXConsumer/src/commonMain/kotlin/libxconsumer/LibXBaseClassUsageExample.kt
@@ -0,0 +1,8 @@
+package libxconsumer
+
+/**
+ *
+ * created by vkorovkin  on 14.10.2020
+ */
+class LibXBaseClassUsageExample : libx.LibXBaseClass("Xyz") {
+}
